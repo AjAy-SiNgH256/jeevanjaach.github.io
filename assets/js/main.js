46a7113a41d979268d62440ea185a0463adf7684
Ajay Singh <xpadajay@gmail.com>
2022-04-09 18:52:14 +0530

    Header and Footer Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,16 +60,16 @@ $(document).ready(function(){
             breakpoint: 1024,
             settings: {
               slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
+              slidesToScroll: 1,
+              infinite: false,
+              dots: false
             }
           },
           {
             breakpoint: 600,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToScroll: 1
             }
           },
           {
