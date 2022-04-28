b0e11457c0c3421be3a7b1a84dee94e77e4fce8c
Ajay Singh <xpadajay@gmail.com>
2022-04-28 16:54:37 +0530

    Banner text aligned to right

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,7 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
+    speed: 300,
     prevArrow: false,
     nextArrow: false,
     autoplay: true,
