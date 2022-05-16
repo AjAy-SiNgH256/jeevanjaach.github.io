848e5c71c3ebfb0f68938c003e29ec7d52800a40
Ajay Singh <xpadajay@gmail.com>
2022-05-16 19:30:04 +0530

    Gallery tabs added

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,14 +48,7 @@
   function init() { var mapOptions = { zoom: 11, scrollwheel: false, navigationControl: false, mapTypeControl: true, scaleControl: false, draggable: true, disableDefaultUI: true, center: new google.maps.LatLng(40.6700, -73.9400), styles: [{ "featureType": "administrative", "elementType": "all", "stylers": [{ "saturation": "-100" }] }, { "featureType": "administrative.province", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 65 }, { "visibility": "on" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": "50" }, { "visibility": "simplified" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": "-100" }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "all", "stylers": [{ "lightness": "30" }] }, { "featureType": "road.local", "elementType": "all", "stylers": [{ "lightness": "40" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "saturation": -100 }, { "visibility": "simplified" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "hue": "#ffff00" }, { "lightness": -25 }, { "saturation": -97 }] }, { "featureType": "water", "elementType": "labels", "stylers": [{ "lightness": -25 }, { "saturation": -100 }] }] }; var mapElement = document.getElementById('xs-map'); var map = new google.maps.Map(mapElement, mapOptions); var marker = new google.maps.Marker({ position: new google.maps.LatLng(40.6700, -73.9400), map: map, title: 'Snazzy!' }); }
 })(jQuery);
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
-    loop: true,
-    autoplaySpeed: 1000,
-    items: 2,
-    autoplay: true
-  });
+  
   $('.testimonial-slider').slick({
     dots: false,
     infinite: true,
