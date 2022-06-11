1d5bc05c10a896e58e79ffe3b627bd27ac1b84ad
Ajay Singh <xpadajay@gmail.com>
2022-06-11 13:33:11 +0530

    counselling section added and font-size managed

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,4 +107,12 @@ $(document).ready(function () {
     $('.tab-switch-block-items').removeClass('active-tab');
     $(this).addClass('active-tab');
   });
+  $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+    e.preventDefault();
+    $(this).siblings('a.active').removeClass("active");
+    $(this).addClass("active");
+    var index = $(this).index();
+    $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+    $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+});
 })
